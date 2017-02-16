584a2013212218d7b441f3e313f4563b397c205c
Nachiket Londhe <londhe.nachiket@gmail.com>
2017-02-15 23:17:30 -0500

    Leetcode file added

---
+++ leetCode/DeleteNode.java
@@ -0,0 +1,99 @@
+package leetCode;
+
+/**
+ * Created by nachiketlondhe on 2/15/17.
+ */
+public class DeleteNode {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int i) {
+            this.val = i;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public TreeNode deleteNode(TreeNode root, int val) {
+        if (root == null) return null;
+
+        TreeNode parent = null;
+        TreeNode temp = root;
+        TreeNode child;
+
+        boolean is_left = true;
+
+        // find the node
+        while (temp != null){
+            if (val == temp.val) {
+                break;
+            } else if (val < temp.val) {
+                parent = temp;
+                is_left = true;
+                temp = temp.left;
+            } else {
+                parent = temp;
+                is_left = false;
+                temp = temp.right;
+            }
+        }
+
+        if (temp == null) {
+            return root;
+        }
+
+        // case 1: this is a leaf node
+        if (temp.left == null && temp.right == null) {
+            if (parent == null) {
+                return null;
+            } else {
+                if (is_left) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+
+        } else if (temp.left == null || temp.right == null) {
+            // case 2: this has exactly only one child
+
+            if (temp.left != null) {
+                child = temp.left;
+            } else {
+                child = temp.right;
+            }
+
+            if (parent == null) {
+                root = child;
+            } else {
+                if (is_left) {
+                    parent.left = child;
+                } else {
+                    parent.right = child;
+                }
+            }
+        } else {
+            // case 3: this one has two children
+            TreeNode parent_repl = temp;
+            TreeNode replacement = temp.left;
+
+            is_left = true;
+            while (replacement.right != null) {
+                parent_repl = replacement;
+                replacement = replacement.right;
+                is_left = false;
+            }
+
+            temp.val = replacement.val;
+
+            if (is_left) {
+                temp.left = replacement.left;
+            } else {
+                parent_repl.right = replacement.left;
+            }
+        }
+
+        return root;
+    }
+}
