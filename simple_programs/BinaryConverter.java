68bc0a99fe7644775e9d59054cd26cf7ab3aa2df
Nachiket Londhe <londhe.nachiket@gmail.com>
2017-02-28 19:33:52 -0500

    adding binary converter

---
+++ simple_programs/BinaryConverter.java
@@ -0,0 +1,50 @@
+package simple_programs;
+
+/**
+ * Class to do simple binary conversions
+ * 1. binary to decimal
+ * 2. decimal to binary
+ * Created by nachiketlondhe on 2/18/17.
+ */
+public class BinaryConverter {
+
+    public static void main(String args[]) {
+
+        BinaryConverter solution = new BinaryConverter();
+
+        solution.binaryToDecimal(1000);
+
+        solution.decimalToBinary(8);
+    }
+
+    /**
+     * 0100
+     */
+    public int binaryToDecimal(int binary) {
+        int power = 0;
+        int decimal = 0;
+
+        while (binary != 0) {
+            decimal += (binary%10)*Math.pow(2, power);
+            binary = binary / 10;
+            power++;
+        }
+
+        return decimal;
+    }
+
+    public void decimalToBinary(int decimal) {
+        int[] binary = new int[50];
+
+        int index = 0;
+        while (decimal != 0) {
+            binary[index] = decimal % 2;
+            decimal = decimal / 2;
+            index++;
+        }
+
+        for (int i  = index; i >= 0; i--) {
+            System.out.print(binary[i] + "");
+        }
+    }
+}
